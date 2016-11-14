63b6b7105549eb1aa0bba5600fcac3125b156f42
wind2013 <825038797@qq.com>
2016-11-14 23:02:52 +0800

    add for android debug

---
+++ com/zhouhao2/debug/MyDebuger.java
@@ -0,0 +1,26 @@
+package com.zhouhao2.debug;
+
+public class MyDebuger {
+
+	public static String getST() {
+		StackTraceElement[] stacks = Thread.currentThread().getStackTrace();
+	    StringBuffer bf= new StringBuffer();
+		for (int i = 2; i < stacks.length; i++) {
+			bf.append(stacks[i].toString()+"\n");
+		}
+		return bf.toString();
+	}
+	
+	public static int getLineNumber(){
+		return Thread.currentThread().getStackTrace()[2].getLineNumber();
+	}
+	
+	public static String getMethodName(){
+       return Thread.currentThread().getStackTrace()[2].getMethodName().toString();
+	}
+	
+	public static String getClassName(){
+		return Thread.currentThread().getStackTrace()[2].getClassName().toString();
+	}
+}
+
